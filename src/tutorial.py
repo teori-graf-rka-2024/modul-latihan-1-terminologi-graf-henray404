2a9e6a6457a06d9733d10ea4068175036d360c20
henray404 <henryalifian13@gmail.com>
2025-03-16 17:55:20 +0700

    Update tutorial.py

--- src/tutorial.py
+++ src/tutorial.py
@@ -1,8 +1,8 @@
 import graph as g
 
-edges = [(1, 2), (1, 3), (2, 4), (2, 5), (3, 6), (3, 7), (4, 8), (5, 9), (6, 10), (7, 11), (8, 12), (9, 12), (10, 12), (11, 12)]
+edge = [(1, 2), (1, 3), (2, 4), (2, 5), (3, 6), (3, 7), (4, 8), (5, 9), (6, 10), (7, 11), (8, 12), (9, 12), (10, 12), (11, 12)]
 
-graph = g.create_graph(edges)
+graph = g.create_graph(edge)
 dfs_result = g.dfs_traversal(graph, 4)
 bfs_result = g.bfs_traversal(graph, 3)
 path = g.find_shortest_path(graph, 2, 8)
